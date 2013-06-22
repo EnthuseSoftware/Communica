139acb7c180f81caea54ffba5479cb44023128d3
Dilshod <dilshod06@gmail.com>
2013-06-22 13:07:12 -0400

    clean todos list

--- LangInformGUI/LangInformGUI/TODOS.cs
+++ LangInformGUI/LangInformGUI/TODOS.cs
@@ -7,8 +7,6 @@ namespace LangInformGUI
 {
     class TODOS
     {
-        //TODO: Creating vocabulary for activities must be on VM
-        //TODO: Prepare the program to James's presentation
-        //TODO: Exam from many lessons.
+        
     }
 }
